413b1cccb3dd63e4343de509ec27f6f10a3fce9d
yvargaspotes-bit <yvargaspotes@gmail.com>
2026-02-09 14:28:26 -0500

    Add files via upload

---
+++ Ejercicios en clase PED.py
@@ -0,0 +1,24 @@
+#calculo de la superficie de un cuadrado conociendo uno de sus lados
+
+lado= 5
+superficie= lado*lado
+print("la superficie del cuadrado es:",superficie)
+
+#Asigna dos numeros a variables y realiza las operaciones basicas(suma, resta, multiplicacion y division)
+
+num1 = 12
+num2 = 5
+
+suma=num1+num2
+resta=num1-num2
+multiplicación=num1*num2
+división=num1/num2
+
+print("Suma:",suma)
+print("Resta:",resta)
+print("Multiplicación:", multiplicación)
+print("División:",división)
+
+
+
+
